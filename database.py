e9c9dbb88e17f9eb3f7aae986c88b0f007ce1f56
zhanghongbin <zhanghongbin@youzan.com>
2022-06-24 19:09:26 +0800

    init

---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+import os
+
+name = os.environ.get("db_name", "test")
+user = os.environ.get("db_user", "-")
+password = os.environ.get("db_password", "-")
+host = os.environ.get("db_host", "-")
+port = os.environ.get("db_port", 3306)
+
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{password}@{host}:{port}/{name}"
+engine = create_engine(SQLALCHEMY_DATABASE_URI, poolclass=NullPool)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(64), unique=True, index=True)
+    hashed_password = Column(String(64))
+    is_active = Column(Boolean, default=True)
+
+    items = relationship("Item", back_populates="owner")
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(64), index=True)
+    description = Column(String(64), index=True)
+    owner_email = Column(String(64), ForeignKey("users.email"))
+
+    owner = relationship("User", back_populates="items")
